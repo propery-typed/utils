0fd9f5dc81336a7d00e2d91c3b2b4342f3c4ea32
temoncher <temoncher@yandex.ru>
2020-12-05 17:39:30 +0300

    add isunknown guard

--- src/guards.ts
+++ src/guards.ts
@@ -2,3 +2,7 @@ export type IfAny<T, Y, N> = 0 extends (1 & T) ? Y : N;
 export type IsAny<T> = IfAny<T, true, false>;
 
 export type IsNever<T> = [T] extends [never] ? true : false;
+
+export type IsUnknown<T> = IsNever<keyof T> extends true
+  ? true
+  : false;
